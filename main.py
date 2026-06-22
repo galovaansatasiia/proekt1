d6dfd308c26d40deaae6926c581a38bbddff7dad
galovaansatasiia <gataylina@gmail.com>
2026-06-23 00:52:32 +0200

    Fix output message formatting in main.py

--- main.py
+++ main.py
@@ -6,4 +6,5 @@ if "Marek" in uzivatel and uzivatel["Marek"] == "1234":
     zprava = "Ahoj Marek, vítej v aplikaci! Pokračuj..."
 else:
     zprava = "Uživatelské jméno nebo heslo nejsou v pořádku!"
+    
 print(f"Výstup: {zprava}")
